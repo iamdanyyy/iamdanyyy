7469875dce136596b323de8bb71c7d1c7cbb8d45
iamdanyyy <94602548+iamdanyyy@users.noreply.github.com>
2023-01-21 10:18:11 +0300

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,18 @@
+public class Main{
+ public static void main(String[] args) {
+  System.out.println("Amdany ni bazenga pro max ");
+  System.out.println("Pia yeye ako na pesa ");
+  System.out.println(3+56);
+   String name = "Amdany";
+    System.out.println(name);
+  final int myNum = 15;
+  System.out.println(myNum);
+  System.out.println("Hello " + name);
+  String secondName = "Kiptum";
+    String fullName = name + " " + secondName;
+  System.out.println(fullName);
+  int wed = 21;
+  int qwed = 49;
+  System.out.println(wed + qwed);
+ }
+}
